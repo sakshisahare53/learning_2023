a8ac8e30b4bd285a38a5d75c9babe5db9c981f0f
sakshisahare53 <sakshisahare53@gmail.com>
2023-06-18 14:41:14 +0530

    Day3

---
+++ Module 1/Challenge1/Challenge1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#define MAX_PACKET_DATA_LENGTH (50)
+
+typedef struct data_packet_t {
+    uint8_t id;
+    uint8_t data_length;
+    uint8_t data[MAX_PACKET_DATA_LENGTH];
+    uint16_t crc;
+} data_packet_t;
+
+uint16_t calculateCRC(const uint8_t *data, size_t length) {
+    uint16_t crc = 0xFFFF;  
+    
+    for (size_t i = 0; i < length; i++) {
+        crc ^= (uint16_t)data[i];  
+        
+        for (int j = 0; j < 8; j++) {
+            if (crc & 0x0001) {
+                crc >>= 1;
+                crc ^= 0xA001;  
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+    
+    return crc;
+}
